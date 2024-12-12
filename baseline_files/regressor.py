c91257813c3f7b9097964fe42f173d6b48d23255
asilyanov <asilyanov@npomis.ru>
2024-12-12 12:55:57 +0000

    first commit

---
+++ baseline_files/regressor.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from sklearn.base import RegressorMixin
+
+class MeanRegressor(RegressorMixin):
+    # Predicts the mean of y_train
+    def fit(self, X=None, y=None):
+        '''
+        Parameters
+        ----------
+        X : array like, shape = (n_samples, n_features)
+        Training data features
+        y : array like, shape = (_samples,)
+        Training data targets
+        '''
+        y = np.array(y)
+
+        self.target = np.mean(y)
+        return self
+
+    def predict(self, X=None):
+        '''
+        Parameters
+        ----------
+        X : array like, shape = (n_samples, n_features)
+        Data to predict
+        '''
+        return np.full(X.shape[0], fill_value=self.target)
